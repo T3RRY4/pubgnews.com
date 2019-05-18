d62a0b1745ffc7eac94e9018b403a261f4c1949e
T3RRY4 <T3RRY4@users.noreply.github.com>
2019-05-18 16:22:09 +0400

    1

---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="header__logo"><a href="#"><img src="./img/logo.svg" alt="Логотип"></a></div>
+    <div class="header__nav">
+        <nav>
+            <ul>
+                <li><a href="#">Главная</a></li>
+                <li><a href="#">Новости</a></li>
+                <li><a href="#">Гайды</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=1920, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PUBGNEWS.COM - Новости PUBG</title>
+    <link rel="stylesheet" href="./css/style.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700&amp;subset=cyrillic" rel="stylesheet">
+</head>
+<body>
+    <?php include="./header.php" ?>
+    <main>
+        <div class="container">
+            <div class="col-left">
+                <div class="content">
+                    <h3>Последние новости</h1>
+                </div>
+            </div>
+            <div class="col-center">
+                <div class="content">
+                    <div class="hot-news">
+                        <img src="./img/news/2nd-phase-pel-13-may.png" alt="Изображение" class="hot-news__img">
+                        <div class="hot-news__headline">
+                            <div class="headline_tags">
+                                <span class="time">1 час назад</span>
+                                <span class="icon">HOT</span>
+                            </div>
+                            <div class="name">
+                                <h3 class="headline">Вторая фаза PEL Contenders стартует 13 мая</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-right">
+                <div class="content">
+                    <h3>Гайды</h3>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
